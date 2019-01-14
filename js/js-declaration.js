8db92591ed2e7a3fc746b50b632dd5be791274da
pjhee <jhee@famnote.com>
2019-01-14 09:57:38 +0900

    190114

---
+++ js/js-declaration.js
@@ -0,0 +1,37 @@
+// JavaScript 선언(Declaration)
+
+// 선언의 3가지 방벙
+// var, let, const 키워드 사용
+
+// 기억하고 싶은 데이터 유형을 변수(variable)에 참조(할당)한다. [reference | assignment]
+
+// 데이터 유형(Types)
+// 숫자형 데이터(Number) = 2018
+
+console.log(2018);
+
+// 올해 = current year
+
+// 변수 선언
+// var에서 -나 띄어쓰기는 오류가 나므로 _를 사용하거나(권장) 대문자로 구분해준다.
+var currentYear; // camelCase
+var last_year; // snakeCase
+
+console.log(currentYear);
+console.log(last_year);
+
+// 값(literal)
+// undefined, null
+
+// 값 할당(assignment)
+// 대입, 할당 연산자(operator) : =
+
+var x, y;
+
+x = 9;
+y = x * x + 10; //91
+
+console.log('x:', x);
+console.log('y:', y);
+
+
